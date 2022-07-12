a72906aebbe9542c954e381feab2e5da41805785
mira-kine <mirak0419@gmail.com>
2022-07-12 16:37:28 -0700

    form constructed, labels added, add item function next

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,44 @@
 export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+	return (
+		<div>
+			<form>
+				<div>
+					<label for="item-name">
+						Item Name:
+						<input
+							type="text"
+							name="item-name"
+							id="item-name"
+							placeholder="name of item"
+						/>
+					</label>
+				</div>
+				<div>
+					<label for="soon">
+						<input
+							type="radio"
+							value=""
+							checked="checked"
+							name="time-frame"
+							id="soon"
+						/>
+						Soon
+					</label>
+				</div>
+				<div>
+					<label for="kind-of-soon">
+						<input type="radio" name="time-frame" id="kind-of-soon" />
+						Kind of Soon
+					</label>
+				</div>
+				<div>
+					<label for="not-soon">
+						<input type="radio" name="time-frame" id="not-soon" />
+						Not Soon
+					</label>
+				</div>
+				<button>Add Item</button>
+			</form>
+		</div>
+	);
 }
