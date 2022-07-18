e2db3bb22dffef4b04476d841d1cbf4352f7a9a2
Trevor Rezac <trev.rezac@gmail.com>
2022-07-18 10:11:06 -0700

    set up search filter input, saving progress on handleSearch function

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,39 @@
 import { ListItem } from '../components';
+import { useState } from 'react';
 
 export function List({ data }) {
+	const [searchQuery, setSearchQuery] = useState('');
+	const [searchResults, setSearchResults] = useState([]);
+
+	function handleSearch(e) {
+		e.preventDefault();
+		setSearchQuery(e.target.value);
+		let filteredResults = [...searchResults];
+		console.log('filteredResults', filteredResults);
+		console.log('data', data);
+
+		filteredResults = data.filter((item) => console.log('item', item));
+
+		setSearchResults(filteredResults);
+	}
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<form>
+				<label htmlFor="search-items-input">
+					Search Items:
+					<input
+						name="search-items"
+						id="search-items"
+						type="text"
+						placeholder="Search items"
+						value={searchQuery}
+						onChange={handleSearch}
+					/>
+				</label>
+			</form>
 			<ul>
 				{data.map((item) => (
 					<ListItem key={item.id} name={item.name} />
