24da67c663bd48c3aa0fa3da579ad20ea68f6898
Trevor Rezac <trev.rezac@gmail.com>
2022-07-19 15:32:29 -0700

    added .toLowerCase back to function, also added it to the query

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -11,7 +11,9 @@ export function List({ data }) {
 	// render onto page search Results -> setting whatever is filtered into searchResults
 
 	function filterResults(query) {
-		return data.filter((item) => item.name.includes(query));
+		return data.filter((item) =>
+			item.name.toLowerCase().includes(query.toLowerCase()),
+		);
 	}
 
 	useEffect(() => {
