2943fadd455258dc57694a3990babd6c8b2bc755
Nadia Zubko <ne.nadezda@gmail.com>
2022-07-25 11:07:08 -0600

    Started creating handlePurchaseITem function, created isPurchased state

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,9 +1,26 @@
 import './ListItem.css';
+import { useState } from 'react';
 
 export function ListItem({ name }) {
+	const [isPurchased, setIsPurchased] = useState(false);
+
+	const handlePurchaseItem = () => {
+		setIsPurchased(true);
+		// set 24 hour time
+		// import and call updateItem on Firebase
+		// update isChecked and nextPurchasedDate snd totalPurchases
+	};
+
+	console.log(isPurchased);
+
 	return (
 		<div className="ListItem">
-			<input type="checkbox" checked id={`${name}-checkbox`} name={name} />
+			<input
+				type="checkbox"
+				id={`${name}-checkbox`}
+				name={name}
+				onChange={handlePurchaseItem}
+			/>
 			<label htmlFor={`${name}-checkbox`}>{name}</label>
 		</div>
 	);
