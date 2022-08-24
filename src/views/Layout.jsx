6e1cbfa6c3ccac5ee0cfab0c7e2f3faea259e0e8
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-08-23 18:16:17 -0700

    Add logo on layout page, update background color in index

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,21 +1,18 @@
-import { Outlet, NavLink } from 'react-router-dom';
-
 import './Layout.css';
-
-/**
- * TODO: The links defined in this file don't work!
- *
- * Instead of anchor element, they should use a component
- * from `react-router-dom` to navigate to the routes
- * defined in `App.jsx`.
- */
+import { Outlet, NavLink } from 'react-router-dom';
+import logo from '../../src/assets/shop-ade.png';
 
 export function Layout({ listToken }) {
 	return (
 		<>
 			<div className="Layout">
 				<header className="Layout-header">
-					<h1>Smart shopping list</h1>
+					<h1>
+						<img
+							src={logo}
+							alt="Bright yellow and orange logo that says SHOP-ADE in all caps."
+						/>
+					</h1>
 				</header>
 				<main className="Layout-main">
 					<Outlet />
