0687e8046cde5fda3cd767826f8bc0383fc129a2
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-08-25 19:54:03 -0700

    Remove SHOP-ADE image from all pages, rendering only on home page

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -6,23 +6,10 @@ export function Layout({ listToken }) {
 	return (
 		<>
 			<div className="Layout">
-				<header className="Layout-header">
-					<h1>
-						<img
-							src={logo}
-							alt="Bright yellow and orange logo that says SHOP-ADE in all caps."
-						/>
-					</h1>
-				</header>
 				<main className="Layout-main">
 					<Outlet />
 				</main>
 				<nav className="Nav">
-					{!listToken && (
-						<NavLink to="/" className="Nav-link">
-							Home
-						</NavLink>
-					)}
 					{listToken && (
 						<>
 							<NavLink to="/list" className="Nav-link">
