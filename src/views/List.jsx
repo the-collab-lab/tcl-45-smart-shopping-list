ac2fd47e07aa6c90a575bbe56e225b26a9d243ef
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-08-27 17:54:36 -0700

    Remove extra logOut button

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -95,6 +95,8 @@ export function List({ data, listToken, loading, confirmLogOut }) {
 				<p>Your list is loading...</p>
 			) : (
 				<>
+<<<<<<< HEAD
+=======
 					<button onClick={handleCopy}>
 						{!copy ? <span>Copy List Name</span> : <span>Copied!</span>}
 					</button>
@@ -107,6 +109,7 @@ export function List({ data, listToken, loading, confirmLogOut }) {
 						Your list name is{' '}
 						<span style={{ color: 'salmon' }}>{listToken}</span>.
 					</p>
+>>>>>>> a7df4d4 (Remove extra logOut button)
 					{data.length >= 1 ? (
 						<>
 							<h3>Find what you're looking for!</h3>
