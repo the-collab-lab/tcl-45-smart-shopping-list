28d24e251fd88a94b230726ef1756ffc38ef2364
Nadia Zubko <ne.nadezda@gmail.com>
2022-08-31 15:07:26 -0600

    Removed unnecessary comments, fixed prop passing to About component

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,6 @@ export function App() {
 	const [copy, setCopy] = useState(false);
 	const [about, setAbout] = useState(false);
 
-	/**
-	 * Here, we're using a custom hook to create `listToken` and a function
-	 * that can be used to update `listToken` later.
-	 *
-	 * `listToken` is `my test list` by default so you can see the list
-	 * of items that was prepopulated for this project.
-	 * You'll later set it to `null` by default (since new users do not
-	 * have tokens), and use `setListToken` when you allow a user
-	 * to create and join a new list.
-	 */
 	const [listToken, setListToken] = useStateWithStorage(
 		null,
 		'tcl-shopping-list-token',
@@ -42,23 +32,8 @@ export function App() {
 	useEffect(() => {
 		if (!listToken) return;
 
-		/**
-		 * streamListItems` takes a `listToken` so it can commuinicate
-		 * with our database; then calls a callback function with
-		 * a `snapshot` from the database.
-		 *
-		 * Refer to `api/firebase.js`.
-		 */
 		return streamListItems(listToken, (snapshot) => {
-			/**
-			 * Read the documents in the snapshot and do some work
-			 * on them, so we can save them in our React state.
-			 *
-			 * Refer to `api/firebase.js`
-			 */
 			const nextData = getItemData(snapshot);
-
-			/** Finally, we update our React state. */
 			setData(nextData);
 			setLoading(false);
 		});
@@ -137,9 +112,14 @@ export function App() {
 					/>
 					<Route
 						path="/about"
-						element={<About />}
-						about={about}
-						setAbout={setAbout}
+						element={
+							<About
+								listToken={listToken}
+								setListToken={setListToken}
+								about={about}
+								setAbout={setAbout}
+							/>
+						}
 					/>
 				</Route>
 			</Routes>
