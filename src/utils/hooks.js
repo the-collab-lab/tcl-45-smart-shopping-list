e25680b30086daa0af9975988a9b6512bac17b65
Nadia Zubko <ne.nadezda@gmail.com>
2022-09-12 16:17:54 -0600

    added useMediaQueryHook to hooks

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -18,3 +18,19 @@ export function useStateWithStorage(initialValue, storageKey) {
 	}, [storageKey, value]);
 	return [value, setValue];
 }
+
+export function useMediaQuery(query) {
+	const [matches, setMatches] = useState(false);
+
+	useEffect(() => {
+		const media = window.matchMedia(query);
+		if (media.matches !== matches) {
+			setMatches(media.matches);
+		}
+		const listener = () => setMatches(media.matches);
+		window.addEventListener('resize', listener);
+		return () => window.removeEventListener('resize', listener);
+	}, [matches, query]);
+
+	return matches;
+}
